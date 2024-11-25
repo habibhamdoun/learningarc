5fcf7ea30bedfc62cf887bee0dd3a66fbccd2c10
habibhamdoun <habibhamdoun@gmail.com>
2024-11-26 01:00:11 +0200

    fixed routing problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import "./App.css";
-import HomePage from "./components/homepage/HomePage";
-import Nav from "./components/homepage/Nav";
+import { Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Nav from './components/homepage/Nav';
+import AuthPage from './components/Login/AuthPage';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div>
       <Nav />
       <Routes>
-        <Route path={`/`} element={<Navigate to="/home" replace />} />
+        <Route path={`/`} element={<Navigate to='/home' replace />} />
         <Route path={`/home`} element={<Home />} />
-        <Route path="/courses" element={<Courses />} />
-        {/* <Route path='/Login' element={<Login />} /> */}
+        {/* <Route path="/courses" element={<Courses />} /> */}
+        <Route path='/auth' element={<AuthPage />} />
       </Routes>
     </div>
   );
 }
-document.documentElement.setAttribute("data-theme", "mytheme");
+document.documentElement.setAttribute('data-theme', 'mytheme');
 export default App;
