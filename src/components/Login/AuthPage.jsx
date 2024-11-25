6a50076e9fcbf83a9cc98733094484c8f1cd09ce
habibhamdoun <habibhamdoun@gmail.com>
2024-11-26 01:08:55 +0200

    small css changes to auth page

--- src/components/Login/AuthPage.jsx
+++ src/components/Login/AuthPage.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
-import login from "../../assets/login.png";
-import register from "../../assets/register.png";
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import login from '../../assets/login.png';
+import register from '../../assets/register.png';
 
 const AuthPage = () => {
   const [isLogin, setIsLogin] = useState(true);
@@ -13,27 +13,27 @@ const AuthPage = () => {
   };
 
   return (
-    <div className="flex h-screen bg-base-100 items-center">
-      <div className="hidden lg:flex w-1/2 items-center justify-end pr-16">
+    <div className='flex h-screen bg-base-100 items-center'>
+      <div className='hidden lg:flex w-1/2 items-center justify-end pr-16'>
         <img
           src={isLogin ? login : register}
-          alt={isLogin ? "Login" : "Register"}
-          className="max-w-lg rounded-lg shadow-lg"
+          alt={isLogin ? 'Login' : 'Register'}
+          className='max-w-lg rounded-lg shadow-lg'
         />
       </div>
-      <div className="w-full lg:w-1/2 flex items-center justify-center px-8">
-        <div className="card bg-white w-full max-w-md rounded-lg">
-          <div className="card-body">
-            <h2 className="text-2xl font-bold text-center mb-6">
+      <div className='w-full lg:w-1/2 flex items-center justify-center px-8'>
+        <div className='card bg-white w-full max-w-md rounded-lg'>
+          <div className='card-body'>
+            <h2 className='text-3xl font-bold text-center mb-6 text-accent'>
               Welcome to Learning Arc
             </h2>
-            <div className="flex justify-center gap-4 mb-6">
+            <div className='flex justify-center gap-4 mb-6'>
               <button
                 onClick={() => setIsLogin(true)}
                 className={`w-1/2 py-2 rounded-lg font-medium transition ${
                   isLogin
-                    ? "bg-primary text-white shadow-md"
-                    : "bg-gray-200 text-gray-500 hover:bg-gray-300"
+                    ? 'bg-primary text-white shadow-md'
+                    : 'bg-gray-200 text-gray-500 hover:bg-gray-300'
                 }`}
               >
                 Login
@@ -42,8 +42,8 @@ const AuthPage = () => {
                 onClick={() => setIsLogin(false)}
                 className={`w-1/2 py-2 rounded-lg font-medium transition ${
                   !isLogin
-                    ? "bg-primary text-white shadow-md"
-                    : "bg-gray-200 text-gray-500 hover:bg-gray-300"
+                    ? 'bg-primary text-white shadow-md'
+                    : 'bg-gray-200 text-gray-500 hover:bg-gray-300'
                 }`}
               >
                 Register
@@ -51,101 +51,105 @@ const AuthPage = () => {
             </div>
             {isLogin ? (
               <form>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text font-medium">Username</span>
+                <div className='form-control'>
+                  <label className='label'>
+                    <span className='label-text font-medium text-accent'>
+                      Username
+                    </span>
                   </label>
                   <input
-                    type="email"
-                    placeholder="Enter your username"
-                    className="input input-bordered focus:border-primary focus:outline-none"
+                    type='email'
+                    placeholder='Enter your username'
+                    className='input input-bordered focus:border-primary focus:outline-none'
                     required
                   />
                 </div>
-                <div className="form-control mt-4">
-                  <label className="label">
-                    <span className="label-text font-medium">Password</span>
+                <div className='form-control mt-4'>
+                  <label className='label'>
+                    <span className='label-text font-medium text-accent'>
+                      Password
+                    </span>
                   </label>
-                  <div className="relative">
+                  <div className='relative'>
                     <input
-                      type={passwordVisible ? "text" : "password"}
-                      placeholder="Enter your password"
-                      className="input input-bordered w-full focus:border-primary focus:outline-none"
+                      type={passwordVisible ? 'text' : 'password'}
+                      placeholder='Enter your password'
+                      className='input input-bordered w-full focus:border-primary focus:outline-none'
                       required
                     />
                     <button
-                      type="button"
-                      className="absolute inset-y-0 right-3 flex items-center text-gray-500 hover:text-gray-800"
+                      type='button'
+                      className='absolute inset-y-0 right-3 flex items-center text-gray-500 hover:text-gray-800'
                       onClick={togglePasswordVisibility}
                     >
                       <FontAwesomeIcon
                         icon={passwordVisible ? faEyeSlash : faEye}
-                        className="w-5 h-5"
+                        className='w-5 h-5'
                       />
                     </button>
                   </div>
-                  <label className="label mt-4">
+                  <label className='label mt-4'>
                     <a
-                      href="#"
-                      className="label-text-alt link link-hover hover:text-primary transition-all"
+                      href='#'
+                      className='label-text-alt text-red-500 link link-hover hover:text-primary transition-all'
                     >
                       Forgot password?
                     </a>
                   </label>
                 </div>
-                <div className="form-control mt-6">
-                  <button className="btn btn-primary w-full">Login</button>
+                <div className='form-control mt-6'>
+                  <button className='btn btn-primary w-full'>Login</button>
                 </div>
               </form>
             ) : (
               <form>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text font-medium">Email</span>
+                <div className='form-control'>
+                  <label className='label'>
+                    <span className='label-text font-medium'>Email</span>
                   </label>
                   <input
-                    type="email"
-                    placeholder="Enter your email"
-                    className="input input-bordered focus:border-primary focus:outline-none"
+                    type='email'
+                    placeholder='Enter your email'
+                    className='input input-bordered focus:border-primary focus:outline-none'
                     required
                   />
                 </div>
-                <div className="form-control mt-4">
-                  <label className="label">
-                    <span className="label-text font-medium">Username</span>
+                <div className='form-control mt-4'>
+                  <label className='label'>
+                    <span className='label-text font-medium'>Username</span>
                   </label>
                   <input
-                    type="text"
-                    placeholder="Enter your username"
-                    className="input input-bordered focus:border-primary focus:outline-none"
+                    type='text'
+                    placeholder='Enter your username'
+                    className='input input-bordered focus:border-primary focus:outline-none'
                     required
                   />
                 </div>
-                <div className="form-control mt-4">
-                  <label className="label">
-                    <span className="label-text font-medium">Password</span>
+                <div className='form-control mt-4'>
+                  <label className='label'>
+                    <span className='label-text font-medium'>Password</span>
                   </label>
-                  <div className="relative">
+                  <div className='relative'>
                     <input
-                      type={passwordVisible ? "text" : "password"}
-                      placeholder="Enter your password"
-                      className="input input-bordered w-full focus:border-primary focus:outline-none"
+                      type={passwordVisible ? 'text' : 'password'}
+                      placeholder='Enter your password'
+                      className='input input-bordered w-full focus:border-primary focus:outline-none'
                       required
                     />
                     <button
-                      type="button"
-                      className="absolute inset-y-0 right-3 flex items-center text-gray-500 hover:text-gray-800"
+                      type='button'
+                      className='absolute inset-y-0 right-3 flex items-center text-gray-500 hover:text-gray-800'
                       onClick={togglePasswordVisibility}
                     >
                       <FontAwesomeIcon
                         icon={passwordVisible ? faEyeSlash : faEye}
-                        className="w-5 h-5"
+                        className='w-5 h-5'
                       />
                     </button>
                   </div>
                 </div>
-                <div className="form-control mt-6">
-                  <button className="btn btn-primary w-full">Register</button>
+                <div className='form-control mt-6'>
+                  <button className='btn btn-primary w-full'>Register</button>
                 </div>
               </form>
             )}
