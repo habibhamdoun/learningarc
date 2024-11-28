d6f35cd37c1669bdb85d299675801e5b2fcf0a42
mhmdmarshoud34 <mhmdmarshoud34@gmail.com>
2024-11-28 18:07:20 +0200

    background changed for contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import image7 from "../../assets/image7.png";
+import image13 from "../../assets/image13.jpeg";
 import Form from "./Form";
 
 const Contact = () => {
   return (
     <>
-      <div className="hero min-h-screen bg-white">
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: `url(${image13})`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content flex justify-center items-center text-neutral-content">
           <div className="card w-full max-w-5xl shadow-xl rounded-xl bg-white transition-transform transform hover:scale-105 mb-12 mt-12">
             <div className="grid lg:grid-cols-2">
@@ -16,6 +23,7 @@ const Contact = () => {
                   className="max-w-full h-auto rounded-lg shadow-lg"
                 />
               </div>
+
               <Form />
             </div>
           </div>
