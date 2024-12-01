3c909cee89160459320f7a67c2cdd3b3f54d73de
habibhamdoun <habibhamdoun@gmail.com>
2024-12-01 20:16:32 +0200

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Nav from './components/NavBar/Nav';
@@ -8,34 +7,30 @@ import HomePage from './components/homepage/HomePage';
 import AboutUs from './components/AboutUs/AboutUs';
 import Footer from './components/General/Footer';
 import Course from './components/Course/Course';
-import Contact from "./components/Contact/Contact";
-import Footer from "./components/General/Footer";
-import AdminPage from "./components/Dashboard/AdminPage";
-import AddCourse from "./components/Dashboard/pages/AddCourse";
-
-
+import Contact from './components/Contact/Contact';
+import AdminPage from './components/Dashboard/AdminPage';
+import AddCourse from './components/Dashboard/pages/AddCourse';
 
 function App() {
   return (
-    <div className="overflow-x-hidden">
+    <div className='overflow-x-hidden'>
       <Nav />
       <Routes>
-        <Route path={`/`} element={<Navigate to="/home" replace />} />
+        <Route path={`/`} element={<Navigate to='/home' replace />} />
         <Route path={`/home`} element={<HomePage />} />
 
-        <Route path="/courses" element={<CoursesPart />} />
+        <Route path='/courses' element={<CoursesPart />} />
         <Route path='/course/:id' element={<Course />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/aboutus" element={<AboutUs />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/admin/add" element={<AddCourse />} />
-
+        <Route path='/auth' element={<AuthPage />} />
+        <Route path='/aboutus' element={<AboutUs />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/admin' element={<AdminPage />} />
+        <Route path='/admin/add' element={<AddCourse />} />
       </Routes>
       <Footer />
     </div>
   );
 }
 
-document.documentElement.setAttribute("data-theme", "mytheme");
+document.documentElement.setAttribute('data-theme', 'mytheme');
 export default App;
--- src/components/General/CourseCard.jsx
+++ src/components/General/CourseCard.jsx
@@ -4,29 +4,29 @@ import calendar from '../../assets/calendarIcon2.svg';
 const CourseCard = ({ id, imgSrc, title, desc, instructor, date, width }) => {
   return (
     <div className={`card bg-base-100 ${width} shadow-xl`}>
-      <a href={`/course/${id}`} className='block'>
-        <figure>
-          <img src={imgSrc} alt='Course' />
-        </figure>
-        <div className='card-body items-center md:items-stretch'>
-          <h2 className='card-title md:text-xl text-base '>{title}</h2>
-          <div className='flex justify-between gap-3 '>
-            <p className='italic text-gray-500 md:text-base text-sm flex-grow-0'>
-              {instructor}
+      <figure>
+        <img src={imgSrc} alt='Course' />
+      </figure>
+      <div className='card-body items-center md:items-stretch'>
+        <h2 className='card-title md:text-xl text-base '>{title}</h2>
+        <div className='flex justify-between gap-3 '>
+          <p className='italic text-gray-500 md:text-base text-sm flex-grow-0'>
+            {instructor}
+          </p>
+          <div className='flex gap-1 items-center justify-center'>
+            <img src={calendar} alt='' className='w-4 h-4 md:block hidden' />
+            <p className='flex-grow-0 opacity-65 md:text-base text-sm'>
+              {date}
             </p>
-            <div className='flex gap-1 items-center justify-center'>
-              <img src={calendar} alt='' className='w-4 h-4 md:block hidden' />
-              <p className='flex-grow-0 opacity-65 md:text-base text-sm'>
-                {date}
-              </p>
-            </div>
-          </div>
-          <p>{desc}</p>
-          <div className='card-actions justify-end'>
-            <button className='btn btn-primary'>Check it Out</button>
           </div>
         </div>
-      </a>
+        <p>{desc}</p>
+        <div className='card-actions justify-end'>
+          <a href={`/course/${id}`} className='block'>
+            <button className='btn btn-primary'>Check it Out</button>
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
