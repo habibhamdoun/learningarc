e8ecf930b65e8da6207e6fc417674ba14bcddf2a
habibhamdoun <habibhamdoun@gmail.com>
2024-12-10 17:56:25 +0200

    added get courses , add course and remove course

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/students', studentRoutes);
-app.use('/api/course', courseRoutes);
+app.use('/api/courses', courseRoutes);
 
 // Start the server
 app.listen(PORT, () => {
