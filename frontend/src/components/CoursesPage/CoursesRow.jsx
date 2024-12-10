fd437f875c2ada69924b89f80c94c3d10d76aead
habibhamdoun <habibhamdoun@gmail.com>
2024-12-10 18:26:06 +0200

    fetching courses from the database

--- frontend/src/components/CoursesPage/CoursesRow.jsx
+++ frontend/src/components/CoursesPage/CoursesRow.jsx
@@ -2,18 +2,18 @@ import PropTypes from 'prop-types';
 import CourseCard from '../General/CourseCard';
 
 const CoursesRow = ({ courses, isCarousel, width }) => {
-  console.log(courses);
-
   return (
     <>
       {courses.map((course) => {
+        console.log(course);
+
         return (
           <div
             className={`${isCarousel ? 'carousel-item' : ''}`}
             key={course.course_id}
           >
             <CourseCard
-              id={course.course_id}
+              id={course.courseID}
               title={course.title}
               desc={course.description}
               instructor={course.instructor}
--- frontend/src/components/homepage/CoursesSection.jsx
+++ frontend/src/components/homepage/CoursesSection.jsx
@@ -1,12 +1,26 @@
-import coursesData from '../../../data.json';
 import CoursesCarousel from '../CoursesPage/CoursesCarousel';
-
+import { getCourses } from '../../services/courseService';
+import { useEffect, useState } from 'react';
 const CoursesSection = () => {
+  const [courses, setCourses] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getCourses();
+        setCourses(data);
+      } catch (error) {
+        console.error('Failed to fetch courses:', error);
+      }
+    };
+
+    fetchData();
+    // console.log(courses);
+  }, []);
   return (
     <div className='overflow-x-hidden'>
       <CoursesCarousel
         title={'Popular Courses'}
-        coursesData={coursesData.courses}
+        coursesData={courses}
         width='w-96'
       />
     </div>
